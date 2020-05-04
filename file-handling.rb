597b313391f2609c8b0300ef54078ed42ff47c6b
Rajan Pandey <rajanpandey2508@gmail.com>
2020-05-05 04:35:42 +0530

    code refactir

--- file-handling.rb
+++ file-handling.rb
@@ -1,21 +1,18 @@
 #!/usr/bin/ruby
 
 # File should be in the same directory
-# Eg of file name: 'README.md', 'file.txt' etc
+
+FILE_TO_READ_FROM = ''.freeze
+FILE_TO_WRITE_INTO = ''.freeze
+BODY_TO_BE_WRITTEN = ''.freeze
 
 def read_text_from_file
-  file_to_read_from = ''
-  File.read(file_to_read_from)
+  File.read(FILE_TO_READ_FROM)
 end
 
 def write_text_into_file
-  file_to_write_into = ''
-  body_to_be_written = ''
-  File.open("#{__dir__}/#{file_to_write_into}", 'w') do |file|
-    file.write(body_to_be_written)
-  end
+  File.write(FILE_TO_WRITE_INTO, BODY_TO_BE_WRITTEN, mode: 'a')
 end
 
-# Call whichever function is required
-# read_text_from_file
-# write_text_into_file
+read_text_from_file
+write_text_into_file
