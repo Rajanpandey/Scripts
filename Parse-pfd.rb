7187459247d98058d3586485f4b060bfe55d2a5c
Rajan Pandey <rajanpandey2508@gmail.com>
2020-05-04 15:35:38 +0530

    pdf parser

---
+++ Parse-pfd.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/ruby
+
+# gem install pdf-reader
+require 'rubygems'
+require 'pdf/reader'
+
+# Pdf file in the same directory
+filename = ""
+
+PDF::Reader.open(filename) do |reader|
+  reader.pages.each do |page|
+    puts page.text
+  end
+end
