ae5b4df5d91e0719d52eb8378f2e2c7e249530e7
Rajan Pandey <rajanpandey2508@gmail.com>
2020-05-05 04:02:10 +0530

    raised exception if file is not supported

--- words-frequency-in-a-doc.rb
+++ words-frequency-in-a-doc.rb
@@ -1,11 +1,20 @@
 #!/usr/bin/ruby
 
-# Script to count frequency of every keyword in a document in descending order
+# Script to count frequency of every keyword in a pdf or txt file in
+# descending order which occurs more than MIN_NO_OF_OCCURRENCES times
 
-def count_words(string)
-  frequency = Hash.new(0)
-  string.split(' ').each { |word| frequency[word.downcase] += 1 }
-  frequency.sort_by(&:last).reverse.to_h
+FILE_TO_PARSE = ''.freeze
+MIN_NO_OF_OCCURRENCES = 1
+ACCEPTED_DOC_EXTENTIONS = ['.pdf', '.txt'].freeze
+
+def sort_and_reject_uncommon_words(dictionary)
+  dictionary.sort_by(&:last).reverse.to_h.select { |_, value| value >= MIN_NO_OF_OCCURRENCES }
+end
+
+def hash_of_word_count(string)
+  dictionary = Hash.new(0)
+  string.split(' ').each { |word| dictionary[word.downcase] += 1 }
+  dictionary
 end
 
 def parse_pdf(pdf_file_to_parse)
@@ -22,13 +31,17 @@ def parse_pdf(pdf_file_to_parse)
 end
 
 def parse_file(file_to_parse)
-  return File.read(file_to_parse) unless File.extname(file_to_parse) == '.pdf'
+  return File.read(file_to_parse) if File.extname(file_to_parse) == '.txt'
 
   parse_pdf(file_to_parse)
 end
 
-# File can be any text file or a pdf
-file_to_parse = ''
-body = parse_file(file_to_parse)
+def validate
+  raise 'Provide file name' if FILE_TO_PARSE.strip.empty?
+  raise 'File not supported' unless ACCEPTED_DOC_EXTENTIONS.include?(File.extname(FILE_TO_PARSE))
+end
 
-pp count_words(body)
+validate
+body = parse_file(FILE_TO_PARSE)
+dictionary = hash_of_word_count(body)
+pp sort_and_reject_uncommon_words(dictionary)
