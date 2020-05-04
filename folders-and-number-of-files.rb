c3ea44ad4850aacdab27f34563eda8758ca8fd9b
Rajan Pandey <rajanpandey2508@gmail.com>
2020-05-04 15:00:37 +0530

    code structure improved

--- folders-and-number-of-files.rb
+++ folders-and-number-of-files.rb
@@ -12,11 +12,9 @@ list_of_folders.reject! { |folder_name| folder_name.include?('.') }
 
 folder_file_count = {}
 list_of_folders.map { |folder_name| 
-    folder_file_count[folder_name] = Dir[File.join(folder_name, '**', '*')].count { |file| 
-        File.file?(file) 
-    } 
+    folder_file_count[folder_name] = Dir[File.join(folder_name, '**', '*')].count { |file| File.file?(file) } 
 }
 
 folder_file_count.each { |folder_name, file_count| 
-    p "Folder #{folder_name} has #{file_count} files" 
+    puts "Folder #{folder_name} has #{file_count} files" 
 }
