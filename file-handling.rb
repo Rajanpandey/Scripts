81a374b2485d16b5bf0e4bf56ceedbfa2883a81e
Rajan Pandey <rajanpandey2508@gmail.com>
2020-05-04 14:56:50 +0530

    file handling

---
+++ file-handling.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/ruby
+
+#File should be in the same directory
+#Eg of file name: 'README.md', 'file.txt' etc
+file_to_read_from = ''
+body = File.read(file_to_read_from)
+
+#File should be in the same directory
+#Eg of file name: 'README.md', 'file.txt' etc
+file_to_write_into = ''
+body_to_be_written = ''
+File.open("#{__dir__}/#{file_to_write_into}", 'w') { |file| file.write(body_to_be_written) }
