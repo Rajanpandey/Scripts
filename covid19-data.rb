fe5d972e03c60e64731abbf8afd588363288af28
Rajan Pandey <rajanpandey2508@gmail.com>
2021-05-02 10:03:10 +0530

    Fixed states with missing district

--- covid19-data.rb
+++ covid19-data.rb
@@ -5,41 +5,42 @@ require 'json'
 
 NO_OF_DISTRICTS_TO_FETCH = 64
 MIN_NUMBER_OF_CASES = 0
-CASE_TYPE = %w[confirmed active recovered deceased].freeze
+CASE_TYPES = %w[confirmed active recovered deceased].freeze
+STATES_WITH_MISSING_DISTRICT_DATA = ['Andaman and Nicobar Islands', 'Assam', 'Goa', 'Manipur', 'Sikkim', 'Telangana'].freeze
+
 URL = 'https://api.covid19india.org/state_district_wise.json'.freeze
 uri = URI(URL)
 response = Net::HTTP.get(uri)
 DISTRICT_WISE_DATA = JSON.parse(response).freeze
-UNAVAILABLE_DATA_DISTRICTS = %w[Telangana Manipur Assam].freeze
 
 def parse_district_wise_data(case_type)
-  delhi_cases_count = 0
-  districts_with_min_num_of_cases = {}
+  district_case_count = {}
   DISTRICT_WISE_DATA.each do |state, state_data|
-    state_data['districtData'].each do |district, district_data|
-      next if UNAVAILABLE_DATA_DISTRICTS.include?(state)
-      districts_with_min_num_of_cases[district] = district_data[case_type] if state != 'Delhi' && district_data[case_type] > MIN_NUMBER_OF_CASES
-      delhi_cases_count += district_data[case_type] if state == 'Delhi'
+    if STATES_WITH_MISSING_DISTRICT_DATA.include?(state)
+      state_case_count = state_data['districtData'].values.map { |val| val[case_type] }.reduce(&:+)
+      district_case_count[state] = state_case_count if state_case_count > MIN_NUMBER_OF_CASES
+    else
+      state_data['districtData'].each do |district, district_data|
+        district_case_count[district] = district_data[case_type] if district_data[case_type] > MIN_NUMBER_OF_CASES
+      end
     end
   end
-  districts_with_min_num_of_cases['Delhi'] = delhi_cases_count if delhi_cases_count > MIN_NUMBER_OF_CASES
-  districts_with_min_num_of_cases.sort_by(&:last).reverse.to_h
-end
-
-def fill_cell(data)
-  data ? "#{data[0].ljust(25)} #{data[1].to_s.rjust(6)}" : ' '.ljust(31)
+  district_case_count.sort_by(&:last).reverse.to_h
 end
 
 def table(confirmed, active, recovered, deceased)
   table_string = '_' * 169
 
+  # Header
   table_string += "\n|   Index   |"
-  CASE_TYPE.each do |case_type|
+  CASE_TYPES.each do |case_type|
     table_string += "   #{case_type.capitalize.ljust(25)} #{'Count'.rjust(6)}   |"
   end
 
   table_string += "\n|#{'-' * 167}|\n"
 
+  # Body
+  def fill_cell(cell_data) "#{cell_data[0].ljust(24)} #{cell_data[1].to_s.rjust(7)}" end
   (0..NO_OF_DISTRICTS_TO_FETCH - 1).each do |row|
     table_string += "|   #{(row + 1).to_s.ljust(5)}   "
     table_string += "|   #{fill_cell(confirmed[row])}   "
@@ -51,8 +52,6 @@ def table(confirmed, active, recovered, deceased)
   table_string += '_' * 169
 end
 
-confirmed, active, recovered, deceased = CASE_TYPE.map do |case_type|
-  parse_district_wise_data(case_type)
-end
-
+# Main
+confirmed, active, recovered, deceased = CASE_TYPES.map { |case_type| parse_district_wise_data(case_type) }
 puts table(confirmed.to_a, active.to_a, recovered.to_a, deceased.to_a)
